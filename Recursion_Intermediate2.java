343787a0de630b339caf7eeb1948568f588828d9
Mohammed Saqlain <120318579+Sq1754@users.noreply.github.com>
2023-01-05 22:10:33 +0530

    Add files via upload

---
+++ Recursion_Intermediate2.java
@@ -0,0 +1,37 @@
+
+public class Recursion_Intermediate2 {
+// Q-> Find the first and last Occurrence of an element in String
+	// example "abaacdsafaah"  elemnet = a
+	//  first occurrence at index 1
+	//  last occurrence at index 10
+	
+  public static int first =-1;// we declared first as constant
+  public static int last = -1;// same for last
+ 
+   public static void findOccur(String str, int idx, char element) {
+	  
+	   if(idx==str.length()) {
+		   return;
+	   }
+	   char currentChar = str.charAt(idx);
+	   if(currentChar==element) {
+		   if(first==-1) {
+			   first =idx;
+		   }
+		   else {
+			   last = idx;
+		   }
+	   }
+	   findOccur(str,idx+1,element);
+	   
+   }
+   
+   public static void main(String args[]) {
+	   String str = "abaacdsafaah";
+	   char element = 'a';
+	   findOccur(str,0,element);
+	   System.out.println("First Occurrence: "+ first);
+	   System.out.println("Last Occurrence: "+ last);
+   }
+}
+
